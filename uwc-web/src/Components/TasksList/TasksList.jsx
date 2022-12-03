3403f45d89933eeac1c409867c74dbb91bd63157
lonelyknight2902 <truongthinh2902@gmail.com>
2022-12-04 01:45:57 +0700

    Add styling to Task view

--- uwc-web/src/Components/TasksList/TasksList.jsx
+++ uwc-web/src/Components/TasksList/TasksList.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import TasksRow from '../TasksRow/TasksRow'
+import './TasksList.css'
 
 function TasksList() {
   return (
     <div>
-        <TasksRow name="Collect garbage" type="Janitor's task" assigned="Nguyen Van A" mcp={[1]} status="completed"/>
+      <div className='label'>
+        <div>Name</div>
+        <div>Type</div>
+        <div>Assigned to</div>
+        <div>Assigned MCPs</div>
+        <div>Status</div>
+      </div>
+        <TasksRow name="Collect garbage" type="Janitor's task" assigned="Nguyen Van A" mcps={[1,2,3]} status="Completed"/>
+        <TasksRow name="Pickup garbage" type="Janitor's task" assigned="Nguyen Van B" mcps={[1]} status="In-progress"/>
     </div>
   )
 }
--- uwc-web/src/Components/TasksRow/TasksRow.jsx
+++ uwc-web/src/Components/TasksRow/TasksRow.jsx
@@ -3,22 +3,24 @@ import './TasksRow.css'
 import { FaUserCircle } from 'react-icons/fa';
 import { IconContext } from "react-icons";
 
-function TasksRow({name, type, assigned, mcp, status}) {
+function TasksRow({name, type, assigned, mcps, status}) {
   return (
     <div className='task'>
         <div className='task-item'>{name}</div>
         <div className='task-item'>{type}</div>
-        <div className='task-item'>
+        <div className='task-item assigned'>
             <IconContext.Provider value={{ size: "32px" }}>
-            <span>
+            <span className='icon'>
                 <FaUserCircle/>
             </span>
             </IconContext.Provider>
             <span>{assigned}</span>
             </div>
-        <div className='task-item'>{mcp}</div>
+        <div className='task-item mcp'>
+          {mcps.map(element => (<div className='circle'>{element}</div>))}
+        </div>
         <div className='task-item'>
-            <div className={status}>{status}</div>
+            <div className={status + ' status'}>{status}</div>
         </div>
     </div>
   )
