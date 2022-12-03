69deccab01810eb63d8a6ebb31a774065bf07d89
Thinh Do <thinhdo@pop-os.localdomain>
2022-12-04 02:27:38 +0700

    Add login form

--- uwc-web/src/App.js
+++ uwc-web/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Dashboard from './Pages/Dashboard/Dashboard';
+import Login from './Pages/Login/Login';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <Dashboard/>
+      <Routes>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
