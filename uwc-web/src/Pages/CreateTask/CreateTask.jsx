e6537e3f83e021b3593262798f52e2d052326ec3
pqtran512 <tran.pham560@hcmut.edu.vn>
2022-12-05 01:06:12 +0700

    to branch createTask

--- uwc-web/src/Pages/CreateTask/CreateTask.jsx
+++ uwc-web/src/Pages/CreateTask/CreateTask.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import './CreateTask.css';
 
 function CreateTask() {
   return (
-    <div>CreateTask</div>
+    <div className='create-task'>
+      <h1>Create janitor's task</h1>
+      <div className='left-panel'>
+        <form>
+          <div className="auth_input">
+            <label htmlFor="task-name">Name</label>
+            <input
+              type="text"
+              placeholder="Enter task name"
+            ></input>
+          </div>
+          <div className="auth_input">
+            <label htmlFor="assigned-to">Assigned to</label>
+            <input
+              type="text"
+              placeholder="+ Add assigned janitor"
+            ></input>
+          </div>
+          <div className="auth_input">
+            <label htmlFor="assigned-mcp">Assigned MCP</label>
+            <input
+              type="text"
+              placeholder="+ Add assigned MCP"
+            ></input>
+          </div>
+          <div className="submit">
+            <button>+ Add task</button>
+          </div>
+        </form>
+      </div>
+      {/* <div className='right-picture'>
+      </div> */}
+      <img src="https://otonari.tokyo/media/wp-content/uploads/2021/04/messageImage_1617255133080-min-768x514.jpg" alt="CreateTask" className='image'></img>
+    </div>
   )
 }
 
